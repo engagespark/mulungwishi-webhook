818376aedbaaf36fdb6d6eee3570fc5e686be2ca
Mike <admiral96>
2016-04-11 16:07:03 +0800

    Enable hourly forecast

--- app/views.py
+++ app/views.py
@@ -16,7 +16,8 @@ def show_user_input():
     sms_to = request.args.get('to')
 
     valid_query_message = "Content: {}  SMS From: {}    SMS To: {}".format(content, sms_from, sms_to)
-    invalid_query_message = "You've entered an incorrect/empty query. Please check and try again. You can try '/query?content=sms_content&from=number_it_came_from&to=number_it_will_go_to'"
+    invalid_query_message = "You've entered an incorrect/empty query. Please check and try again.\
+                             You can try '/query?content=sms_content&from=number_it_came_from&to=number_it_will_go_to'"
     invalid_query_status_code = 400
 
     return valid_query_message if content and sms_from and sms_to else (invalid_query_message, invalid_query_status_code)
@@ -30,14 +31,23 @@ def generate_forecast():
 
     geocode = Geocode(address=place)
     is_place_valid = geocode.is_place_query_valid()
+
     if not is_place_valid:
         return 'No results found. Try a more generic place name i.e. local, province', 400
 
+    frequency = 'currently'  # default forecast set to current
+    valid_frequency = ['currently', 'hourly']
+    last_query = place.split(' ')[-1].lower()
+    if last_query in valid_frequency:
+        place = place.replace(last_query, '')  # replace time method(if present) with empty string so that only the place remains. Example query: cebu hourly
+        frequency = last_query
+
     forecast = WeatherForecast()
     coordinates = geocode.get_coordinates()
-    weather_forecast = forecast.generate_forecast(latitude=coordinates['lat'], longitude=coordinates['lng'])
+    weather_forecast = forecast.generate_forecast(latitude=coordinates['lat'], longitude=coordinates['lng'], frequency=frequency)
     place_info = geocode.get_place_info()
-    return 'WEATHER FORECAST FOR {}: \nPlace Type: {}\n{}'.format(place_info['formatted_address'].upper(), place_info['place_type'], display(forecast=weather_forecast))
+    return 'WEATHER FORECAST FOR {}: \nPlace Type: {}\n{}'.\
+        format(place_info['formatted_address'].upper(), place_info['place_type'], display(forecast=weather_forecast, frequency=frequency))
 
 
 @url.errorhandler(404)
@@ -59,5 +69,18 @@ def convert_to_readable_time(time):
     return time.strftime('%a %b %d,%Y %I:%M:%S %p')
 
 
-def display(forecast):
-    return 'Time: {}\nTemperature: {}°C\nHumidity: {}\nProbability of Precipitation: {}\nWeather Summary: {}'.format(convert_to_readable_time(forecast.time), round(forecast.temperature, 2), forecast.humidity, forecast.precipProbability, forecast.summary)
+def display(forecast, frequency):
+    if frequency == 'currently':
+        return 'Time: {}\nTemperature: {}°C\nHumidity: {}\nProbability of Precipitation: {}\nWeather Summary: {}'.\
+            format(convert_to_readable_time(forecast.time), round(forecast.temperature, 2), forecast.humidity, forecast.precipProbability, forecast.summary)
+
+    info = []
+    for count, item in enumerate(forecast.data):
+        info.append('time: {}'.format(convert_to_readable_time(item.time)))
+        info.append('temperature: {}'.format(item.temperature))
+        info.append('humidity: {}'.format(item.humidity))
+        info.append('precipProbability: {}'.format(item.precipProbability))
+        info.append('summary: {}\n'.format(item.summary))
+        if count > 12:
+            break
+    return '\n'.join(info)
